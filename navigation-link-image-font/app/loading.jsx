35a09295f16d1c18bf51235e90c8b05a47f39de8
Bittu Kumar <bittukumar8713@gmail.com>
2026-03-21 00:17:36 +0530

    add page not found

--- navigation-link-image-font/app/loading.jsx
+++ navigation-link-image-font/app/loading.jsx
@@ -3,8 +3,15 @@ import React from 'react'
 function loading() {
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      {/* <div className="min-h-screen flex items-center justify-center bg-gray-50">
         <div className="h-10 w-10 border-4 border-gray-300 border-t-black rounded-full animate-spin" />
+      </div> */}
+      <div className="min-h-screen flex items-center justify-center bg-gray-50">
+        <div className="flex gap-2">
+          <div className="h-3 w-3 bg-black rounded-full animate-bounce" />
+          <div className="h-3 w-3 bg-black rounded-full animate-bounce [animation-delay:0.2s]" />
+          <div className="h-3 w-3 bg-black rounded-full animate-bounce [animation-delay:0.4s]" />
+        </div>
       </div>
     </>
   );
--- navigation-link-image-font/app/not-found.js
+++ navigation-link-image-font/app/not-found.js
@@ -1,9 +1,26 @@
 import React from "react";
+import Link from "next/link";
 
 function notfound() {
   return (
     <>
-      <div>Not Found</div>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 px-6 text-center">
+        <h1 className="text-7xl font-bold text-gray-900 tracking-tight">404</h1>
+        <p className="mt-4 text-lg text-gray-600">
+          Oops! The page you’re looking for doesn’t exist.
+        </p>
+
+        <p className="mt-2 text-sm text-gray-500">
+          It might have been moved, deleted, or never existed.
+        </p>
+        
+        <Link
+          href="/"
+          className="mt-6 inline-flex items-center justify-center px-6 py-3 rounded-xl bg-black text-white text-sm font-medium hover:bg-gray-800 transition"
+        >
+          Go Back Home
+        </Link>
+      </div>
     </>
   );
 }
